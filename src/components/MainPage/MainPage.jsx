434d73ff2bc7c04a009824b42c8583d62675b3cc
Vladimir Yablokov <YablokovWork@gmail.com>
2022-06-11 11:09:00 +0300

    main container

--- src/components/MainPage/MainPage.jsx
+++ src/components/MainPage/MainPage.jsx
@@ -1,20 +1,24 @@
 import React from 'react'
 import ButtonOrder from '../UI/ButtonOrder/ButtonOrder'
 import s from './MainPage.module.sass'
+import mainImg from '../media/mainimg.png'
 
 export default function MainPage() {
   return (
     <div className={s.main}>
         <div className={s.mainText}>
-            <h4>Welcome To Pages!!!</h4>
-            <h1>Your Books From The Best Writter.</h1>
-            <p>We believe that reading books are essential to a healthy culture.</p>
-            <p>They’re where authors can connect with readers.</p>
-        </div>
-        <div className={s.mainBtns}>
+          <h4>Welcome To Pages!!!</h4>
+          <h1>Your Books From The Best Writter.</h1>
+          <p>We believe that reading books are essential to a healthy culture.</p>
+          <p>They’re where authors can connect with readers.</p>
+          <div className={s.mainBtns}>
             <ButtonOrder/>
-            <div className={s.MainDemoBtn}>Read Free Demo</div>
+            <div className={s.MainDemoBtn}>
+              <p>Read Free Demo</p>
+            </div>
+          </div>
         </div>
+        <img src={mainImg} alt="THE DARK LIGHT" />
     </div>
   )
 }
--- src/components/UI/ButtonNavPages/ButtonNavPages.jsx
+++ src/components/UI/ButtonNavPages/ButtonNavPages.jsx
@@ -5,7 +5,5 @@ import s from './ButtonNavPages.module.sass'
 export default function ButtonNavPages() {
   return (
     <button className={s.btn}></button>
-
-
   )
 }
